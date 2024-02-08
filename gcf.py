724b7f006748567a79d0f3468aeed50443600ddf
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-02-08 13:43:50 -0500

    fjggjgdxsgxjn

--- gcf.py
+++ gcf.py
@@ -1,6 +1,10 @@
 def factors(x,y):
-    for i in range(int(x),int(y)):
-        gcf= int(y)/int(i)
-        if gcf>=int(gcf):
-            print(int(gcf))
+    for i in range(1,int(x)):
+        def factor1(x):
+            int(x)/int(i)
+        def factor2(y): 
+            int(y)/int(i)
+    if factor1==factor2:
+            print(int(factor1))
+
 factors(input("give number"), input("give another number"))
