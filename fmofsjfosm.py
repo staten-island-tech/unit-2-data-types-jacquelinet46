d96de17d36b2c656da15ddf00885d6302ba8aed6
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-03-08 14:00:05 -0500

    cgjcgj

--- fmofsjfosm.py
+++ fmofsjfosm.py
@@ -1,6 +1,6 @@
 def bank():
     accounts = [[1,2,3], [4,5,6,1], [0,7,4,5,4]]
-    m = accounts.length
+    m = accounts.length 
     y = [sum([int(i) for i in account]) for account in accounts]
     n = accounts[i].length
     1 <= m, n <= 50
