5ca475f8dc49d38e48d92f8bd874374586ec990c
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-03-11 13:53:57 -0400

    notes

--- fmofsjfosm.py
+++ fmofsjfosm.py
@@ -1,11 +1,19 @@
-def bank():
-    accounts = [[1,2,3], [4,5,6,1], [0,7,4,5,4]]
-    m = accounts.length 
-    y = [sum([int(i) for i in account]) for account in accounts]
-    n = accounts[i].length
-    1 <= m, n <= 50
-    1 <= accounts[i][j] <= 10    
-    print(y)
-    return
+accounts = [['1','2','3'], ['4','5','6','1']]
 
-bank()
+def bank(accounts):
+    int_accs = [[int(x) for x in i] for i in accounts]
+    largest = 0
+    for money in int_accs:
+        x = sum(money)
+        if x > largest:
+            largest = x
+    print(largest)
+"""     m = accounts.length 
+    y = [sum([int(i) for i in account]) for account in accounts]
+    n = accounts[3].length
+    1 <= m, n <= 50
+    1 <= accounts[3][4] <= 10    
+    print(y)
+    return wrong answer """
+#notes
+bank(accounts)
