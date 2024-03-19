45f0c9069f9c4199bb218faba64d5dbe9909d0ca
jacquelinet46 <jacquelinet46@nycstudents.net>
2024-03-19 13:32:44 -0400

    dfhhfd

---
+++ hgewhgoq.py
@@ -0,0 +1,15 @@
+#classes!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+class Calculator():
+    #encapsulation
+    def add(x,y):
+        return x+y
+    def subtract(x,y):
+        return x-y
+print(Calculator.add(5,6))
+print(Calculator.subtract(5,6))
+
+class Score():
+    def add(x,y):
+        return x+y
+
---
+++ wninei.py
@@ -0,0 +1,3 @@
+from hgewhgoq import Calculator
+print(Calculator.add(77,77))
+import math
